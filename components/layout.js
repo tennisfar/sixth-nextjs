4e3e72f0bf12dfe25488c824ff86df2a53589ee7
Michael L Petersen <ekmlpe@danskespil.dk>
2022-07-22 12:26:51 +0200

    footer should not shrink on small screens

--- components/layout.js
+++ components/layout.js
@@ -14,7 +14,7 @@ export default function Layout({ children, home }) {
 
       <Main>{children}</Main>
 
-      <footer className="h-40 bg-[#F8E81C] p-5 text-center relative bg-[url('/footer-bg.jpg')] flex justify-center">
+      <footer className="h-40 basis-40 shrink-0 bg-[#F8E81C] p-5 text-center relative bg-[url('/footer-bg.jpg')] flex justify-center">
         <p className="text-xs text-[#B4A812] absolute bottom-5 tracking-widest">
           SIXTH.DK (C) 2022 &#8226; CVR 38418998
         </p>
