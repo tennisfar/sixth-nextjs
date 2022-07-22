d0506c8dabd22ec7576fbb2825d3bdb725286306
Michael L Petersen <ekmlpe@danskespil.dk>
2022-07-22 12:23:33 +0200

    fix overflow issue

--- components/layout.js
+++ components/layout.js
@@ -14,8 +14,8 @@ export default function Layout({ children, home }) {
 
       <Main>{children}</Main>
 
-      <footer className="h-40 bg-[#F8E81C] p-5 text-center relative bg-[url('/footer-bg.jpg')]">
-        <p className="text-xs text-[#B4A812] absolute w-full bottom-5 tracking-widest">
+      <footer className="h-40 bg-[#F8E81C] p-5 text-center relative bg-[url('/footer-bg.jpg')] flex justify-center">
+        <p className="text-xs text-[#B4A812] absolute bottom-5 tracking-widest">
           SIXTH.DK (C) 2022 &#8226; CVR 38418998
         </p>
       </footer>
